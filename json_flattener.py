37b1bbf13bf0d3c8f3d9d298e070a13d6d12c2d3
Sako Arts <s@koarts.nl>
2021-02-09 12:10:33 +0100

    added flattend json

---
+++ json_flattener.py
@@ -0,0 +1,19 @@
+import json
+
+canon = json.load(open('ai_canon.json'))
+
+def recurse_json(old_obj):
+    new_obj = []
+    for k, v in old_obj.items():
+        v['name'] = k
+        if 'children' in v:
+            v['children'] = recurse_json(v['children'])
+
+        new_obj.append(v)
+
+
+    return new_obj
+
+flat_canon = recurse_json(canon)
+print(flat_canon)
+json.dump(flat_canon, open('flat_ai_canon.json', 'w'), indent=4)
