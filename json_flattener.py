8d8e789c9bdfed4c50eda16bc8ac4475e4dcdb33
Sako Arts <s@koarts.nl>
2021-02-09 17:33:38 +0100

    failed attempts

--- json_flattener.py
+++ json_flattener.py
@@ -8,12 +8,14 @@ def recurse_json(old_obj):
         v['name'] = k
         if 'children' in v:
             v['children'] = recurse_json(v['children'])
+        else:
+            v['size'] = 3938
 
         new_obj.append(v)
 
 
     return new_obj
 
-flat_canon = recurse_json(canon)
+flat_canon = recurse_json(canon)[0]
 print(flat_canon)
 json.dump(flat_canon, open('flat_ai_canon.json', 'w'), indent=4)
